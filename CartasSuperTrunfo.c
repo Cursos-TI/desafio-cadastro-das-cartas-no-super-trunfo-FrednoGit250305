66aeebece4db6f9dfb6a3d4c18a72af3212afba6
Alfredo no Git <202502009895@alunos.estacio.br>
2025-03-06 22:17:00 +0000

    cadastro de variáveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,13 +3,31 @@
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
 
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+
+    // Definição das variáveis para as duas cartas.
+
+    char estado1 = 'A';
+    int cdg1 = 1;
+    char city1 [20] = "SP";
+    int população1 = 9;
+    float area1 = 8;
+    float pib1 = 7;
+    int locais1 = 6;
+
+    char estado2 = 'B';
+    int cdg2 = 2;
+    char city2 [20]; "BH";
+    int população2 = 5;
+    float area2 = 4;
+    float pib2 = 3;
+    int locais2 = 2;
+
     
+
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
